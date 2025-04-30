f6dd53e53a54f4f759e2ee0c1b82933aaa34f18e
Игнатова Евгения <evgeniiaignatova0@gmail.com>
2025-04-30 17:21:34 +0300

    fix

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ knowledgeBase.forEach(item => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-        <img src="${item.image}" alt="${item.title}" class="card-image" />
         <h3>${item.title}</h3>
         <p>${item.description}</p>
     `;
+
     card.onclick = () => {
         Swal.fire({
             title: item.title,
-            html: `<p>${item.content}</p>`,
+            html: `
+                <img src="${item.image}" alt="${item.title}" class="modal-image" />
+                <p>${item.content}</p>
+            `,
             confirmButtonText: 'ОК'
         });
     };
+
     container.appendChild(card);
 });
