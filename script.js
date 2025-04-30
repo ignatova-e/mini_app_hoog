c655314df63d16288533a0c7daa10a53a847476d
Игнатова Евгения <evgeniiaignatova0@gmail.com>
2025-04-30 17:18:33 +0300

    fix

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const container = document.getElementById("card-container");
 knowledgeBase.forEach(item => {
     const card = document.createElement("div");
     card.className = "card";
-    card.innerHTML = `<h3>${item.title}</h3><p>${item.description}</p>`;
+    card.innerHTML = `
+        <img src="${item.image}" alt="${item.title}" class="card-image" />
+        <h3>${item.title}</h3>
+        <p>${item.description}</p>
+    `;
     card.onclick = () => {
         Swal.fire({
             title: item.title,
