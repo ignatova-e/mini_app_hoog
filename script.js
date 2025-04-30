8ffe66a5356f1805eb9d5bb617369353db62cca3
Игнатова Евгения <evgeniiaignatova0@gmail.com>
2025-04-30 17:49:09 +0300

    fix

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// const container = document.getElementById("card-container");
-
-// knowledgeBase.forEach(item => {
-//     const card = document.createElement("div");
-//     card.className = "card";
-//     card.innerHTML = `
-//         <h3>${item.title}</h3>
-//         <p>${item.description}</p>
-//     `;
-
-//     card.onclick = () => {
-//         Swal.fire({
-//             title: item.title,
-//             html: `
-//                 <img src="${item.image}" alt="${item.title}" class="modal-image" />
-//                 <p>${item.content}</p>
-//             `,
-//             confirmButtonText: 'ОК'
-//         });
-//     };
-
-//     container.appendChild(card);
-// });
-
 const container = document.getElementById("card-container");
 const searchInput = document.getElementById("searchInput");
 
@@ -53,21 +29,22 @@ function displayCards(filteredItems) {
             });
         };
         container.appendChild(card);
-    }
+    });
+}
 
 // Фильтрация по запросу в поле поиска
 searchInput.addEventListener('input', (event) => {
-        const searchTerm = event.target.value.toLowerCase(); // Получаем введенный запрос
-        const filteredKnowledgeBase = knowledgeBase.filter(item => {
-            return (
-                item.title.toLowerCase().includes(searchTerm) ||
-                item.description.toLowerCase().includes(searchTerm) ||
-                item.content.toLowerCase().includes(searchTerm)
-            );
-        });
-
-        displayCards(filteredKnowledgeBase); // Показываем отфильтрованные статьи
+    const searchTerm = event.target.value.toLowerCase(); // Получаем введенный запрос
+    const filteredKnowledgeBase = knowledgeBase.filter(item => {
+        return (
+            item.title.toLowerCase().includes(searchTerm) ||
+            item.description.toLowerCase().includes(searchTerm) ||
+            item.content.toLowerCase().includes(searchTerm)
+        );
     });
 
-    // Изначально выводим все карточки
-    displayCards(knowledgeBase);
+    displayCards(filteredKnowledgeBase); // Показываем отфильтрованные статьи
+});
+
+// Изначально выводим все карточки
+displayCards(knowledgeBase);
