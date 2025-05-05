daf0f482b57ce6d25176d9409e9f49f9cd9c30fb
Evgenia Ignatova <73296478+ignatova-e@users.noreply.github.com>
2025-05-05 11:10:22 +0000

    version 2

--- bot.py
+++ bot.py
@@ -1,7 +1,32 @@
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram import Update, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
+
+
+from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+
 TOKEN = "7257113754:AAEH7m3Fu0eOOMzmNB3Kgz4mtk6j7a33sGA"
+CHANNEL_ID = "@testtestt23e" 
+
+async def send_and_pin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = InlineKeyboardMarkup([[
+        InlineKeyboardButton("📚 База знаний", url="https://ignatova-e.github.io/mini_app_hoog/")
+    ]])
+    sent_msg = await context.bot.send_message(
+        chat_id=CHANNEL_ID,
+        text="📌 Нажми на кнопку, чтобы открыть базу знаний:",
+        reply_markup=keyboard
+    )
+    await context.bot.pin_chat_message(chat_id=CHANNEL_ID, message_id=sent_msg.message_id)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("pin", send_and_pin))  # команда /pin
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
 
 
 # /start — кнопка в личку
